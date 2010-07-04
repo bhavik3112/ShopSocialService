743ef14dd65f5ae61c438e713ad3d555d4455324
Sainath Mallidi <sainathdreams@gmail.com>
2010-07-04 05:22:10 -0700

    updating shouts model with associations

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,8 +1,8 @@
 class Shout < ActiveRecord::Base
 
-    belongs_to :user, 
+    belongs_to :sender, 
         :foreign_key => 'sender', :class_name => 'User'
-    belongs_to :user, 
+    belongs_to :reciever, 
         :foreign_key => 'reciever', :class_name => 'User'
 
 end
